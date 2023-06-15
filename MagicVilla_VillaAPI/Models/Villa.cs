d4ae54fdd9d705b75c19b955253aa058ec96983b
DESKTOP-2QFP1NN\HP <kavit.soni@tatvasoft.com>
2023-06-15 08:43:52 +0530

    - complete First End point

---
+++ MagicVilla_VillaAPI/Models/Villa.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_VillaAPI.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+       
+        public string Name { get; set; }
+    }
+}
